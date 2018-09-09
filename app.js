c10616a87ae55697cab91f2c53b99e6b5155bb45
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-09-09 15:00:06 +0530

    routes added
    routes added

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express     = require('express'),
-      bodyParser  = require('body-parser'),
-      path        = require('path'),
-      expejs      = require('ejs'),
-      mongoose    = require('mongoose');
+const express               = require('express'), 
+      bodyParser            = require('body-parser'), // json
+      path                  = require('path'), // public folder related
+      expejs                = require('ejs'),
+      mongoose              = require('mongoose'),
+      expsession            = require('express-session'), // user related
+      passport              = require('passport'), // user related
+      passportLocal         = require('passport-local'), // user related
+      passportLocalMongoose = require('passport-local-mongoose'); // user related
       
-const app         = express();
+const app                   = express();
 
-app.locals.moment = require('moment');
+app.locals.moment           = require('moment');    // display comment add time
 
+const User                  = require('./models/user');
+
+const userRoute             = require('./routes/usermanage'),
+      commentsRoute         = require('./routes/comments');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -17,11 +25,32 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(`${__dirname}/public`));
 
+
+app.use(expsession ({
+    secret: 'ChuChUyA is the CuTesT CAT evEr!',
+    // resave: false,
+    // saveUninitialized: false // session object will not be stored in the session store
+}));
+
+// To use Passport in an Express, configure it with the required passport.initialize() middleware
+// If your application uses persistent login sessions passport.session()middleware must also be used
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// coming from passportLocalMongoose to tell passport for using local strategy to authenticate user data
+// taking data from sessions which is encoded and unencoded 
+// (the function is from passportLocalMongoose which we have imported
+passport.use(new LocalStrategy(User.authenticate())); 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true });
 
-app.get('/', (req, res) => {
-    res.send('TRYE');
-});
+
+app.use('/user', userRoute);
+app.use('/comment', commentsRoute);
+
 
 const PORT = process.env.PORT || 3000 ;
 app.listen(PORT, () => {
