f5b8018f3bc09082260d8a00883764b4111c22bf
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-09-09 19:01:51 +0530

    add user auth
    add user auth

--- routes/usermanage.js
+++ routes/usermanage.js
@@ -1,15 +1,47 @@
 const express               = require('express'),
       router                = express.Router(),
-      mongoose              = require('mongoose'),
-      passportLocalMongoose = require('passport-local-mongoose');
+      passport              = require('passport');
 
 const User                  = require('../models/user');
 
-router.get('/', (req, res) => {
-    res.send('TRYE');
+router.get('/register', (req, res) => {
+    res.render('register');
 });
 
+router.post('/register', (req, res) => {
+    const username = req.body.username,
+          password = req.body.password;
 
+    User.register(new User({username: username}), password, (err, user) => {
+        if(err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect('/user/login');
+        });
+    });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', passport.authenticate("local" , {
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/register'
+}));
+
+
+router.get('/profile', (req, res) => {
+    //res.send('welcome to profile section');
+    res.render('profile');
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/user/login');
+});
 
 
 module.exports = router;
